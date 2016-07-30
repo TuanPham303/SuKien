dbb71da136c5b0f4514de36d0b86f4b9d46767d7
TuanPham303 <phamcongtuan303@gmail.com>
2016-07-29 23:51:53 -0700

    Revert "Revert "mainPageBody""
    This reverts commit 4d17e778eedf1f3a2af397a2d87e689965936736.

---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Su Kien</title>
+	<!-- Latest compiled and minified CSS & JS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+	<script src="//code.jquery.com/jquery.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="CSS/mainpage-body.css">
+	<link href='https://fonts.googleapis.com/css?family=Poiret+One' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<div class="container">
+		<h1><a href="#">Technology</a></h1>
+		<div class="row">
+			<div class="col-md-4">
+				<a href="#">
+					<div class="event one">
+						<div class="info"><span class="event-name">Learn HTML</span><span class="event-date">1/1/2016</span><br></div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-4">
+				<a href="#">
+					<div class="event two">
+						<div class="info"><span class="event-name">Learn CSS</span><span class="event-date">2/1/2016</span></div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-4">
+				<a href="#">
+					<div class="event three">
+						<div class="info"><span class="event-name">Learn JavaScript</span><span class="event-date">3/1/2016</span></div>
+					</div>
+				</a>
+			</div>
+		</div><br>
+		<h1><a href="#">Education</a></h1>
+		<div class="row">
+			<div class="col-md-4">
+				<a href="#">
+					<div class="event four">
+						<div class="info"><span class="event-name">Study at Canada</span><span class="event-date">4/1/2016</span></div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-4">
+				<a href="#">
+					<div class="event five">
+						<div class="info"><span class="event-name">Study at Rusia</span><span class="event-date">5/1/2016</span></div>
+					</div>
+				</a>
+			</div>
+			<div class="col-md-4">
+				<a href="#">
+					<div class="event six">
+						<div class="info"><span class="event-name">Study at America</span><span class="event-date">6/1/2016</span></div>
+					</div>
+				</a>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
